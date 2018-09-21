49839b8a16ff5cbf1acc13dfdb9e887ec671fd6c
Erin <erin.tregunna1@gmail.com>
2018-09-21 15:00:14 +0100

    first pass at sharing property record

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,7 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/check_your_answers', function(req, res){
+    res.render('check_your_answers')
+})
