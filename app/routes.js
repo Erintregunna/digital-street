aeefced7237f28b713a69ba06e7807888195e4b6
Erin <erin.tregunna1@gmail.com>
2018-09-28 13:54:34 +0100

    check your answer routing

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,7 @@ router.get('/start_page', function(req, res){
 })
 
 router.get('/solicitor_name', function(req, res){
-    res.render('solicitor_name.html')
+    return res.render('solicitor_name.html', {'redirect_url': '/solicitor_name'})
 })
 
 router.post('/solicitor_name', function(req, res){
@@ -17,7 +17,7 @@ router.post('/solicitor_name', function(req, res){
 })
 
 router.get('/solicitor_email', function(req, res){
-    res.render('solicitor_email.html')
+    res.render('solicitor_email.html', {'redirect_url': '/solicitor_email'})
 })
 
 router.post('/solicitor_email', function(req, res){
@@ -25,12 +25,12 @@ router.post('/solicitor_email', function(req, res){
 })
 
 router.get('/reference_number', function(req, res){
-      return res.render('reference_number.html', {'e': false})
+      return res.render('reference_number.html', {'redirect_url': '/reference_number','e': false})
 })
 
 router.post('/reference_number', function(req, res){
     if (req.body['case_number'] =='' ){
-        return res.render('reference_number.html', {'e': true})
+        return res.render('reference_number.html', {'redirect_url': '/reference_number','e': true})
     }
     res.redirect('/check_your_answers')
 })
@@ -48,4 +48,27 @@ router.get('/invited_solicitor', function(req, res){
     res.render('invited_solicitor.html')
 })
 
+router.get('/check_name', function (req,res){
+    return res.render('solicitor_name.html', {'redirect_url': '/check_name'})
+})
+
+router.post('/check_name', function(req, res){
+    res.render('check_your_answers.html')
+})
+
+router.get('/check_email', function (req,res){
+    return res.render('solicitor_email.html', {'redirect_url': '/check_email'})
+})
+
+router.post('/check_email', function(req, res){
+    res.render('check_your_answers.html')
+})
+
+router.get('/check_number', function (req,res){
+    return res.render('reference_number.html', {'redirect_url': '/check_number'})
+})
+
+router.post('/check_number', function(req, res){
+    res.render('check_your_answers.html')
+})
 module.exports = router
